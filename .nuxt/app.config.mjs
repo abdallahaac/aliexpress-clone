498a03f844f73b46f6cf54342e86776b5110b2eb
abdallahaac <aceydesignz@gmail.com>
2023-08-07 07:22:18 -0400

    initial commit

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/abdallahac/Desktop/repo/aliexpress-clone/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
